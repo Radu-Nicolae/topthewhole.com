5bbd1bd3c10730b65a54aef6526b5c6b8f99cd88
Radu-Nicolae <rnicolae04@gmail.com>
2021-01-14 10:38:34 +0200

    Changes be4 wp

---
+++ cool/header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+   <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="x-ua-compatible" content="ie=edge" />
+      <title>Top the Whole</title>
+      <meta name="description" content="" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="manifest" href="site.webmanifest">
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <link rel="stylesheet" href="css/owl.carousel.min.css" />
+      <link rel="stylesheet" href="css/magnific-popup.css" />
+      <link rel="stylesheet" href="css/font-awesome.min.css" />
+      <link rel="stylesheet" href="css/themify-icons.css" />
+      <link rel="stylesheet" href="css/nice-select.css" />
+      <link rel="stylesheet" href="css/flaticon.css" />
+      <link rel="stylesheet" href="css/gijgo.css" />
+      <link rel="stylesheet" href="css/animate.min.css" />
+      <link rel="stylesheet" href="css/slick.css" />
+      <link rel="stylesheet" href="css/slicknav.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" href="css/responsive.css">
+      <link rel="stylesheet" href="css/more.css">
+   </head>
+   <body>
+      
+      <!-- header-start -->
+      <header class="albastru">
+         <div class="header-area albastru global-nav-bar">
+            <div id="sticky-header" class="main-header-area albastru">
+               <div class="container-fluid albastru">
+                  <div class="row align-items-center albastru">
+                     <div class="col-xl-3 col-lg-2 albastru">
+                        <div class="logo albastru">
+                           <a href="index.html">
+                           <img
+                              src="img/googlelogo.png"
+                              class="logo"
+                              alt=""
+                              />
+                           </a>
+                        </div>
+                     </div>
+                     <style>
+                        .hvr:hover {
+                        text-decoration: underline;
+                        }
+                        .albastru {
+                        background-color: #1b9dde;
+                        }
+                     </style>
+                     <div class="col-xl-9 col-lg-10">
+                        <div class="main-menu d-none d-lg-block text-right">
+                           <nav>
+                              <ul id="navigation">
+                                 <li>
+                                    <a class="active hvr" href="index.html">Home</a>
+                                 </li>
+                                 <li>
+                                  <a href="blog.html">Articles</a>
+                                  <ul class="submenu">
+                                     <li>
+                                        <a href="evenimente.html" class="hvr">Type 1</a>
+                                     </li>
+                                     <li>
+                                       <a href="type2.html" class="hvr">Type 2</a>
+                                     </li>
+                                     
+                                  </ul>
+                               </li>
+                                 <li>
+                                    <a href="about.html" class="hvr">About Us</a>
+                                 </li>
+                                 
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                     <div class="col-12 albastru">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- header-end -->
